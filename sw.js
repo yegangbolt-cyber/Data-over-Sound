48533ebfb5cd9e52dc73c77451a554ef1e54d4f0
yegangbolt-cyber <yegangbolt@gmail.com>
2026-08-17 23:21:25 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE='acoustic-transfer-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    let copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
